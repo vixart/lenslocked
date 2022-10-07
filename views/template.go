729d1f0bca1fc3ea1d175d4515d698dc925e5716
sat <77387934+sixsat@users.noreply.github.com>
2022-10-07 22:35:18 +0700

    Add support for multiple patterns in views.ParseFS

--- views/template.go
+++ views/template.go
@@ -39,8 +39,8 @@ func Parse(filePath string) (Template, error) {
 	}, nil
 }
 
-func ParseFS(fs fs.FS, pattern string) (Template, error) {
-	tpl, err := template.ParseFS(fs, pattern)
+func ParseFS(fs fs.FS, patterns ...string) (Template, error) {
+	tpl, err := template.ParseFS(fs, patterns...)
 	if err != nil {
 		return Template{}, fmt.Errorf("parsing template: %w", err)
 	}
