7d1aca2cae985bdec7e827b3052fda9d381cb21f
sat <77387934+sixsat@users.noreply.github.com>
2022-10-15 18:31:18 +0700

    Add user to a database when signing up

--- controllers/user.go
+++ controllers/user.go
@@ -15,8 +15,15 @@ type User struct {
 }
 
 func (u User) Create(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Email: ", r.FormValue("email"))
-	fmt.Fprintln(w, "Password: ", r.FormValue("password"))
+	email := r.FormValue("email")
+	password := r.FormValue("password")
+	user, err := u.UserService.Create(email, password)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "Something went wrong.", http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, "User created: %+v", user)
 }
 
 func (u User) New(w http.ResponseWriter, r *http.Request) {
