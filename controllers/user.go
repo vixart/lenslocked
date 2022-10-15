dda2d192e77131dd7b546d9ef7bc0c68610c6aad
sat <77387934+sixsat@users.noreply.github.com>
2022-10-15 22:59:45 +0700

    Support signing user in

--- controllers/user.go
+++ controllers/user.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/sixsat/lenslocked/models"
@@ -20,7 +21,7 @@ func (u User) Create(w http.ResponseWriter, r *http.Request) {
 	password := r.FormValue("password")
 	user, err := u.UserService.Create(email, password)
 	if err != nil {
-		fmt.Println(err)
+		log.Printf("creating user: %v", err)
 		http.Error(w, "Something went wrong.", http.StatusInternalServerError)
 		return
 	}
@@ -33,6 +34,22 @@ func (u User) New(w http.ResponseWriter, r *http.Request) {
 	}
 	data.Email = r.FormValue("email")
 	u.Templates.New.Execute(w, data)
+}
+
+func (u User) ProcessSignIn(w http.ResponseWriter, r *http.Request) {
+	var data struct {
+		Email    string
+		Password string
+	}
+	data.Email = r.FormValue("email")
+	data.Password = r.FormValue("password")
+	user, err := u.UserService.Authenticate(data.Email, data.Password)
+	if err != nil {
+		log.Printf("processing signin: %v", err)
+		http.Error(w, "Something went wrong.", http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, "User authenticated: %+v", user)
 }
 
 func (u User) SignIn(w http.ResponseWriter, r *http.Request) {
--- main.go
+++ main.go
@@ -57,6 +57,7 @@ func main() {
 	r.Get("/signup", userCtrl.New)
 	r.Post("/user", userCtrl.Create)
 	r.Get("/signin", userCtrl.SignIn)
+	r.Post("/signin", userCtrl.ProcessSignIn)
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
