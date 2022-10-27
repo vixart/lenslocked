cea6d40d14b73481ac2a434e057a780bea7e29cf
sat <77387934+sixsat@users.noreply.github.com>
2022-10-27 17:33:46 +0700

    Add method for hashing the session token

--- models/session.go
+++ models/session.go
@@ -1,7 +1,9 @@
 package models
 
 import (
+	"crypto/sha256"
 	"database/sql"
+	"encoding/base64"
 	"fmt"
 
 	"github.com/sixsat/lenslocked/rand"
@@ -39,14 +41,18 @@ func (ss *SessionService) Create(userID int) (*Session, error) {
 	if err != nil {
 		return nil, fmt.Errorf("create: %w", err)
 	}
-	// TODO: Hash the session token
 	session := Session{
-		UserID: userID,
-		Token:  token,
-		// TODO: Set the TokenHash
+		UserID:    userID,
+		Token:     token,
+		TokenHash: ss.hash(token),
 	}
 	// TODO: Store session in the DB
 	return &session, nil
+}
+
+func (ss *SessionService) hash(token string) string {
+	tokenHash := sha256.Sum256([]byte(token))
+	return base64.URLEncoding.EncodeToString(tokenHash[:])
 }
 
 func (ss *SessionService) User(token string) (*User, error) {
