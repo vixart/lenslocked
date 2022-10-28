f81519ba07b44a92eb1f8f4835fbc4158116972f
sat <77387934+sixsat@users.noreply.github.com>
2022-10-28 16:49:49 +0700

    Implement SessionService.User method

--- models/session.go
+++ models/session.go
@@ -56,7 +56,8 @@ func (ss *SessionService) Create(userID int) (*Session, error) {
 	if err == sql.ErrNoRows {
 		row = ss.DB.QueryRow(`
 			INSERT INTO sessions (user_id, token_hash)
-			VALUES ($1, $2) RETURNING id;`, session.UserID, session.TokenHash,
+			VALUES ($1, $2)
+			RETURNING id;`, session.UserID, session.TokenHash,
 		)
 		err = row.Scan(&session.ID)
 	}
@@ -72,6 +73,25 @@ func (ss *SessionService) hash(token string) string {
 }
 
 func (ss *SessionService) User(token string) (*User, error) {
-	// TODO: Implement SessionService.User
-	return nil, nil
+	tokenHash := ss.hash(token)
+	var user User
+	row := ss.DB.QueryRow(`
+		SELECT user_id
+		FROM sessions
+		WHERE token_hash = $1;`, tokenHash,
+	)
+	err := row.Scan(&user.ID)
+	if err != nil {
+		return nil, fmt.Errorf("user: %w", err)
+	}
+	row = ss.DB.QueryRow(`
+		SELECT email, password_hash
+		FROM users
+		WHERE id = $1;`, user.ID,
+	)
+	err = row.Scan(&user.Email, &user.PasswordHash)
+	if err != nil {
+		return nil, fmt.Errorf("user: %w", err)
+	}
+	return &user, nil
 }
