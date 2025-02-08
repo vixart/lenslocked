c20f4cbece6cf8a4ce3a17bb9f68f5d23e52c776
Alexander Moschenkov <vixmontreal@gmail.com>
2025-02-08 21:38:38 +0300

    Remove comments

--- views/template.go
+++ views/template.go
@@ -51,15 +51,17 @@ func Must(t Template, err error) Template {
 	return t
 }
 
-// func Parse(filePath string) (Template, error) {
-// 	tpl, err := template.ParseFiles(filePath)
-// 	if err != nil {
-// 		return Template{}, fmt.Errorf("parsing template: %w", err)
-// 	}
-// 	return Template{
-// 		htmlTpl: tpl,
-// 	}, nil
-// }
+func Parse(filePath string) (Template, error) {
+	tpl, err := template.ParseFiles(filePath)
+
+	if err != nil {
+		return Template{}, fmt.Errorf("parsing template: %w", err)
+	}
+
+	return Template{
+		htmlTpl: tpl,
+	}, nil
+}
 
 func ParseFS(fs fs.FS, patterns ...string) (Template, error) {
 	tpl := template.New(patterns[0])
